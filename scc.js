8cebcc3972fdf4d58b19a5c10f8625f7dd715c21
soccerJoshNumberNine <blue@smittycraft.com>
2017-08-02 10:56:35 -0700

    Update scc.js

--- scc.js
+++ scc.js
@@ -1,27 +1,9 @@
-var digits;
-var output = [];
-
-var hex = {
-  vals : [
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
-  ],
-  
-  conversion: function (base, number) {
-    switch (base) {
-      case 10:
-        for (digits = 0; (Math.pow(16, digits)) <= number; digits++) {}      
-        for (i = 0; i < digits; i++) {
-          for (j = 0; (j * (Math.pow(16, digits - i - 1))) < number; j++) {
-            
-          }
-          output.push(hex.vals[j]);
-          number -= j * (Math.pow(16, digits - i - 1));
-        }
-        break;
-      case 2:
-        break;
-    }
-    // return digits + 1;
-    return String(digits) + ' <- digits || values -> ' + String(output);
+var $scc = {
+  toHex: function (number) {
+    return {
+      from: function (base) {
+        return parseInt(number, base).toString(16);
+      }
+    };
   }
 };
