5ac5a16d08291cd8ad776f8d07eac551669480a4
soccerJoshNumberNine <blue@smittycraft.com>
2017-08-02 13:07:47 -0700

    Update scc.js

--- scc.js
+++ scc.js
@@ -1,8 +1,21 @@
+// Compatible with bases 3-36. Bases 0, 1, and 2, do not work.
 var $scc = {
   toHex: function (number) {
     return {
       from: function (base) {
         return parseInt(number, base).toString(16);
+      }
+    };
+  },
+  
+  convert: function (number) {
+    return {
+      from: function (startBase) {
+        return {
+          to: function (endBase) {
+            return parseInt(number, startBase).toString(endBase);
+          }
+        };
       }
     };
   }
